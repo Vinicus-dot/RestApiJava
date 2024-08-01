777ad7af71ba5aba7d679880d1490ecc12d169e1
Vini <vinicius.rs97@ufu.br>
2024-08-01 17:47:46 -0300

    add connection using pooling

--- src/main/java/org/example/rest_api/repository/implementations/AnyRepository.java
+++ src/main/java/org/example/rest_api/repository/implementations/AnyRepository.java
@@ -1,12 +1,12 @@
 package org.example.rest_api.repository.implementations;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.example.rest_api.model.entity.ChatMessage;
 import org.example.rest_api.repository.interfaces.IAnyRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.example.rest_api.util.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Repository;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import java.util.List;
@@ -15,20 +15,8 @@ import java.util.List;
 public class AnyRepository implements IAnyRepository {
     private final JdbcTemplate _jdbcTemplate;
 
-    public AnyRepository() {
-        Dotenv dotenv = Dotenv.load();
-        String url = dotenv.get("SPRING_DATASOURCE_PRIMARY_URL");
-        String username = dotenv.get("SPRING_DATASOURCE_PRIMARY_USERNAME");
-        String password = dotenv.get("SPRING_DATASOURCE_PRIMARY_PASSWORD");
-        String driverClassName = dotenv.get("SPRING_DATASOURCE_PRIMARY_DRIVER_CLASS_NAME");
-
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(driverClassName);
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
-
-        this._jdbcTemplate = new JdbcTemplate(dataSource);
+    public AnyRepository() throws Exception {
+        this._jdbcTemplate = DataSource.PrimaryDb();
     }
 
     @Override
---
+++ src/main/java/org/example/rest_api/util/DataSource.java
@@ -0,0 +1,42 @@
+package org.example.rest_api.util;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class DataSource {
+
+    static Dotenv dotenv = Dotenv.load();
+    public static JdbcTemplate PrimaryDb() throws Exception
+    {
+        String url = GetEnvironmentVariable("SPRING_DATASOURCE_PRIMARY_URL");
+        String username = GetEnvironmentVariable("SPRING_DATASOURCE_PRIMARY_USERNAME");
+        String password = GetEnvironmentVariable("SPRING_DATASOURCE_PRIMARY_PASSWORD");
+        String driverClassName = GetEnvironmentVariable("SPRING_DATASOURCE_PRIMARY_DRIVER_CLASS_NAME");
+
+        return  new JdbcTemplate(new HikariDataSource(new HikariConfig(){{
+            setDriverClassName(driverClassName);
+            setJdbcUrl(url);
+            setUsername(username);
+            setPassword(password);
+            setMaximumPoolSize(10);
+            setMinimumIdle(2);
+            setConnectionTimeout(30000);
+            setIdleTimeout(600000);
+        }}));
+    }
+
+    private static String GetEnvironmentVariable(String variable) throws Exception {
+
+        if(variable.isEmpty())
+            throw new Exception("GetEnvironmentVariable not accept variable empty");
+
+        String value = dotenv.get(variable);
+
+        if(value == null || value.isEmpty())
+            throw new Exception("Enviroment Variable "+ variable + " is null or empty");
+
+        return  value;
+    }
+}
